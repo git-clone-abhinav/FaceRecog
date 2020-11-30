ca72f3f7820219be884a57a7d16ceed6f32758d6
abhinavSingh <abhi.anu003@gmail.com>
2020-11-30 11:51:46 +0530

    gitignore

--- facerecog.py
+++ facerecog.py
@@ -6,7 +6,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # To capture video from webcam. 
 cap = cv2.VideoCapture(0)
 # To use a video file as input 
-# cap = cv2.VideoCapture('filename.mp4')
+#cap = cv2.VideoCapture('video.mp4')
 
 while True:
     # Read the frame
@@ -20,7 +20,7 @@ while True:
 
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (199,190,71), 4)
 
     # Display
     cv2.imshow('img', img)
